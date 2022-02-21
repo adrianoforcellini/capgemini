cd43f4da5cfb158023d8fd52204515fb75675485
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2022-02-20 21:26:30 -0300

    Refatoraçao exercicio 3

--- Ex 3/3.js
+++ Ex 3/3.js
@@ -5,10 +5,10 @@ const isAnagram = (str1, str2) => {
 };
 
 const verifyNumberOfSubstringsAnagrams = (str) => {
-  let stringSize = str.length;
+  const stringSize = str.length;
   let growing = 1;
   let countingAnagrams = 0;
-  let arrayOfSubstrings = [];
+  const arrayOfSubstrings = [];
 
   while (growing < stringSize) {
     for (let j = 0; j < stringSize; j++) {
@@ -19,15 +19,14 @@ const verifyNumberOfSubstringsAnagrams = (str) => {
     growing += 1;
   }
 
-  let count = 0;
   for (let k = 0; k < arrayOfSubstrings.length; k++) {
     for (let l = 0; l < arrayOfSubstrings.length; l++) {
       if (isAnagram(arrayOfSubstrings[k], arrayOfSubstrings[l]) && k !== l) {
-        count += 1;
+        countingAnagrams += 1;
       }
     }
   }
-  console.log(count / 2);
+  console.log(countingAnagrams / 2);
 };
 
 verifyNumberOfSubstringsAnagrams(
